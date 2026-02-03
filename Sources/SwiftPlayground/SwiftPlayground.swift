e9fba2b1fa00b48c1d5b08633d3f86bf1a23546f
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-02-03 23:57:37 +0000

    Lunch task beggening

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,16 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        print(1+1)
     }
+}
+
+let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
+let days = [ " 1,", " 2", " 3", "4", "5"]
+
+func food(){
+    for i in lunches{
+    print("\days[i]"): \(lunches[i]")
+    
+} 
 }
