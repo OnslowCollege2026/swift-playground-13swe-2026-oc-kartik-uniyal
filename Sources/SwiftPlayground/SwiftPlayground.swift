1a2bfc7ab1992bdf429a582e4b299cb481b67c69
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-02-12 22:09:38 +0000

    Task finshed

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,27 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-// Average scores to pass
-let ScoresAbove50 = 8
+// Minimum scores to pass
+let MinimumPassingScores = 50
 // Extra points for scores above 50
 let ExtraPoints = 5
-// Minimum oassing scores
-let PassingScore = 50
 @main
 struct SwiftPlayground {
     static func main() {
         let numbers = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
-        // Curving each score by 5
-        let total = numbers.map { number in
-            return number + ExtraPoints
-        // Filtering the number to only accept 50 or more
-        }.filter { number in
-            return number >= PassingScore
-        // Calculating the average scores
-        }.reduce(0) { result, number in
-            return result + number
-        }
+        // Cured the scores then only accepeting scores over 50
+        let total = numbers.map { $0 + ExtraPoints}.filter { $0 >= MinimumPassingScores}
+        // Reduce the passing scores 
+        let sum = total.reduce (0, + )
         // Printing the total score
-        print(total/ScoresAbove50)
+        print(sum/total.count)
     }
 }
