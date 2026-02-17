a1006a28427d4e537949c4fc49a6aef655530f8a
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-02-17 23:59:58 +0000

    Code isnt printing and sill need to do maping add 10 years

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,19 +1,67 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-// Minimum scores to pass
-let MinimumPassingScores = 50
-// Extra points for scores above 50
-let ExtraPoints = 5
+struct Student {
+    let id: Int
+    var name: String
+    var age: Int
+    let nsn: Int
+    var email: String
+    func summary() -> String {
+        return """
+                        ID: \(id)
+                        Name = \(name)
+                        Age = \(age)
+                        NSN = \(nsn)
+                        Email = \(email)
+            """
+    }
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-        let numbers = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
-        // Cured the scores then only accepeting scores over 50
-        let total = numbers.map { $0 + ExtraPoints}.filter { $0 >= MinimumPassingScores}
-        // Reduce the passing scores 
-        let sum = total.reduce (0, + )
-        // Printing the total score
-        print(sum/total.count)
+        let Students = [
+            Student(
+                id: 22346,
+                name: "Kartik Uniyal",
+                age: 17,
+                nsn: 0_145_086_104,
+                email: "kartik.uniyal@student.onslow.school.nz",
+            ),
+            Student(
+                id: 22893,
+                name: "Rapha Diwa",
+                age: 16,
+                nsn: 016_734_055,
+                email: "rapha.diwa@student.onslow.school.nz",
+            ),
+            Student(
+                id: 22361,
+                name: "Sam Thomas",
+                age: 17,
+                nsn: 0_144_467_007,
+                email: "samuel.thomas@student.onslow.school.nz",
+            ),
+            Student(
+                id: 22791,
+                name: "Archie Dom",
+                age: 17,
+                nsn: 147_095_160,
+                email: "archiedom9@gmail.com",
+            ),
+            Student(
+                id: 22410,
+                name: "John Quimpo",
+                age: 17,
+                nsn: 12_345_678,
+                email: "samuel.thomas@student.onslow.school.nz",
+            ),
+        ]
+
+        print("ddyd")
+
     }
 }
+
+SwiftPlayground.main()
