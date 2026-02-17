048d81ad0c8e74df3cb9333569658945c539b1a0
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-02-17 23:10:49 +0000

    All done

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,21 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+// Minimum scores to pass
+let MinimumPassingScores = 50
+// Extra points for scores above 50
+let ExtraPoints = 5
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-        let days = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
+        let numbers = [45, 78, 89, 32, 50, 92, 67, 41, 99, 56]
+        // Cured the scores then only accepeting scores over 50
+        let total = numbers.map { $0 + ExtraPoints}.filter { $0 >= MinimumPassingScores}
+        // Reduce the passing scores 
+        let sum = total.reduce (0, + )
+        // Printing the total score
+        print(sum/total.count)
     }
 }
-
-let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-func CostEachDay(Lunches:[Double] ){
-    for lunches in lunches {
-    print(lunches)
-}
-
-} 
