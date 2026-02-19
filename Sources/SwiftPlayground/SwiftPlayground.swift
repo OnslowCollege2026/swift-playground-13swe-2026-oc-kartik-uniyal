75f9c43cf9917478fff6f5e39c1f096968b77299
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-02-19 22:00:12 +0000

    Finsh of banking then do area checker

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,21 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-        let days = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-    }
 }
 
-let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-func CostEachDay(Lunches:[Double] ){
-    for lunches in lunches {
-    print(lunches)
 }
 
-} 
+struct car {
+    let brand: String
+    let model: String
+    let year: Int
+}
