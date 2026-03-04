661677641a5aad6bb6e902bdf07e23abf12a2486
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-03-04 12:19:16 +0000

    Everything all finshed

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -12,6 +12,8 @@ struct SwiftPlayground {
 print("Car one is a beautiful \(carOne.brand) \(carOne.model) and made in \(carOne.year)")
 print("Car two is a beautiful \(carTwo.brand) \(carTwo.model) and made in \(carTwo.year)")
 
+print("-------------------")
+
 //Bank accounts
 let accountOne = BankAccount(owner: "Kartik", balance: 1000000)
 let accountTwo = BankAccount(owner: "John", balance: 54.653)
@@ -19,10 +21,42 @@ let accountTwo = BankAccount(owner: "John", balance: 54.653)
 // Printing the accounts
 print(accountOne.description())
 print(accountTwo.description())
+
+print("-------------------")
+
+let rectangleOne = rectangle(width: 15.67, hight: 172)
+let rectangleTwo = rectangle(width: 30, hight: 55)
+
+print("Rectangle one's area is \(rectangleOne.area())")
+print("Rectangle two's area is \(rectangleTwo.area())")
+
+if rectangleOne.area() > rectangleTwo.area() {
+    print("Rectangle one is bigger")
+}else {
+    print("Rectanlge two is bigger")
+}
+
+print("-------------------")
+
+let easyQuest = Quest(title: "Collect 10 wood", difficulty: "easy", reward: 10, rank: 1)
+let mediumQuest = Quest(title: "Kill 10 eneimes", difficulty: "Medium", reward: 20, rank: 2)
+let hardQuest = Quest(title: "Kill the final boss", difficulty: "Hard", reward: 30, rank: 3)
+
+print(easyQuest.printBadge())
+print(mediumQuest.printBadge())
+print(hardQuest.printBadge())
+
+
+if easyQuest.rank > mediumQuest.rank && easyQuest.rank > hardQuest.rank {
+    print("Easy quest is the highest")
+} else if mediumQuest.rank > easyQuest.rank && mediumQuest.rank > hardQuest.rank {
+    print("Medium quest is the hardest")
+} else {
+    print("Hard quest is the hardest")
 }
 
 }
-
+}
 
 //Car struct that has brand, model, and year
 struct car {
@@ -37,6 +71,8 @@ struct BankAccount{
 
     func description() -> String {
     return "Owners accoutns are \(self.owner) and the balance is \(self.balance) "
+
+    
 }
 }
 
@@ -44,7 +80,19 @@ struct BankAccount{
 struct rectangle{ 
     var width: Double
     var hight: Double
+
+    func area() -> Double {
+       return (width * hight)
+    }
 }
 
-let rectangleOne = rectangle(width: 15.67, hight: 172)
-let rectangleTwo = rectangle(width: 30, hight: 55)
+struct Quest {
+    var title: String
+    var difficulty : String
+    var reward: Int
+    var rank: Int
+    func printBadge() -> String{
+        return ("\(title) - \(difficulty) - \(reward)")
+
+    }
+}
