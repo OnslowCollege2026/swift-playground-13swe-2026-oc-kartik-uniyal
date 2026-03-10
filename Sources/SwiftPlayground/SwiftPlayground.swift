3f91c03c9825e72c05bd600c63a58df0e54cbb2e
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-03-10 21:43:56 +0000

    Mar 10, 2026, 9:43 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -28,7 +28,7 @@ struct SwiftPlayground {
         $0.name.hasPrefix("m") || $0.name.hasPrefix("w")
     }
 
-    let scores = filterSightings.Map {Int($0)}
+    let scores = filterSightings.Map {$0.scores}
     }
     
 }
