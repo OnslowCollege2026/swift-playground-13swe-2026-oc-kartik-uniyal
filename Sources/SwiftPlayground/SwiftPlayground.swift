ec8e746a03044608a8f2d92b5d855d5e006360ab
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-03-10 23:57:21 +0000

    W database

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,18 +4,5 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-        let days = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-    }
+    } 
 }
-
-let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-func CostEachDay(Lunches:[Double] ){
-    for lunches in lunches {
-    print(lunches)
-}
-
-} 
