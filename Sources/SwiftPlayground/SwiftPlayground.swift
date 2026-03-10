c0c70b24f5260f57cc7d4154e2c6929d1375e103
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-03-10 21:20:46 +0000

    Mar 10, 2026, 9:20 PM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,9 +4,16 @@
 @main
 struct SwiftPlayground {
     static func main() {
-
+    // Task A
+    // List of stuff were given
     let mixed = ["Cat", "7", "Owl", "15", "Dog", "3"]
+
+    // Using compact map to only keep the integers
     let numbers = mixed.compactMap {Int($0)}
+
+    //Printing the numbers
     print(numbers)
+
+    
     }
 }
