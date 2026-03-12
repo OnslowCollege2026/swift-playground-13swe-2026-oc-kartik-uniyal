c592e90dafc5b5e56132ce0c5e5c6d33eeae2196
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-03-12 22:13:39 +0000

    Expanded on the database and made some code

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,8 +1,68 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import GRDB
+import Foundation
+
+/// A reservation at the cafe
+struct Purchaser : Codable, FetchableRecord, PersistableRecord{
+    /// The purchaser ID
+    let id: Int
+
+    /// The name of the customer 
+    var name: String
+
+    /// The number of people at the party/table (minimum 1)
+    var count: Int
+
+    /// The name of the reserved table
+    var reservedTable: String
+
+    enum CodingKeys: String, CodingKey {
+        case id = "purchaser ID"
+        case name = "name"
+        case count = "count"
+        case reservedTable = "reservedTable"
+    }
+}
+
+struct Order : Identifiable, Codable, FetchableRecord, PersistableRecord{
+    /// The order ID
+    let id: Int
+
+    /// The purchaser ID from the purchaser struct
+    var purchaserID: Int
+
+    /// The amount of something
+    var amount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id = "order ID"
+        case purchaserID = "purchaser ID"
+        case amount = "amount"
+    }
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-    } 
+        let dbPath = "Sources/SwiftPlayground/cafe.db Sources/SwiftPlayground/SwiftPlayground.swift"
+        do {
+        let dbQueue = try DatabaseQueue(path: dbPath)
+        print("Database connection successful")
+
+        // Dump the Schema to make sure we are connected to the database file
+        try dbQueue.read { database in
+            try database.dumpSchema()
+            }
+
+    // Find a customer at the window seat
+    let windowSitter = Purchaser.filter(key: [
+        "reservedTable" : "Window Seat"
+    ])
+    print(windowSitter)
+    } catch{
+        print(error)
+    }
+}
 }
