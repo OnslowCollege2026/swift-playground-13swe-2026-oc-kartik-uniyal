5ca5b2a3e37bb592cc93a141a7c714a08eb3d7ae
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-03-19 22:11:34 +0000

    Finshyed task 2

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -83,22 +83,40 @@ struct SwiftPlayground {
     static func main() {
         let dbPath = "Sources/SwiftPlayground/cafe.db"
         do {
-            let dbQueue = try DatabaseQueue(path: dbPath)
+            guard let dbQueue = try? DatabaseQueue(path: dbPath) else {
+                fatalError("Could not open")
+            }
+            // Prints when data base connects
             print("Database connection successful")
 
             // Dump the Schema to make sure we are connected to the database file
             try dbQueue.read { database in
                 try database.dumpSchema()
-                let purchaserID: Int = 1
-                let purchaserID: Int = 100
 
-                let purchaser = try Purchaser.fetchOne(database, key: purchaserID)
-                if let purchaser {
+                // Making the purchaser ID 1
+                let purchaserID: Int = 1
+
+                // Looking through the databasew to find the purchaserID
+                if let purchaser = try Purchaser.fetchOne(database, key: purchaserID){
                     print("Found purchaser \(purchaser.name)")
                 } else {
                     print("No purchaser with Id \(purchaserID) found")
                 }
+
+                // Making a fake ID
+                let fakeID: Int = 100
+
+                // Trying to find the fake ID that dosen't exist in my database
+                if let fakepurchaser = try Purchaser.fetchOne(database, key: fakeID){
+                    print("Found purchaser \(fakepurchaser.name)")
+                } else {
+                    print("No purchaser with Id \(fakeID) found")
+                }
             }
+
+        if let item = try Item.fetchOne(db, id:1) {
+            print(item)
+        }
         } catch {
             print(error)
         }
@@ -111,5 +129,4 @@ struct SwiftPlayground {
     //} catch{
     //    print(error)
     //}
-
 }
