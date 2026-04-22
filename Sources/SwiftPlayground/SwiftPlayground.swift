31711f7ccff105de65b716382ea2fddc309443fc
oc-kartik-uniyal <135765340+oc-kartik-uniyal@users.noreply.github.com>
2026-04-22 00:26:44 +0000

    Start

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,21 +1,5 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// Programming Summataive assesment 
+// Created by Kartik Uniyal
+// Created on 22/04/2026
 
-@main
-struct SwiftPlayground {
-    static func main() {
-        let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-        let days = [ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-    }
-}
-
-let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
-
-func CostEachDay(Lunches:[Double] ){
-    for lunches in lunches {
-    print(lunches)
-}
-
-} 
+print("Hello, World!")
